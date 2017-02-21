b2e5c3f6e17b043ec50bd10425a78bf31fee2f16
mwongeraE <evansmwongera@gmail.com>
2017-02-21 18:48:25 +0300

    DrugDescription.class

--- app/src/main/java/com/mwongera/paediatric_protocols/fragment/Drugsfragment.java
+++ app/src/main/java/com/mwongera/paediatric_protocols/fragment/Drugsfragment.java
@@ -1,31 +1,18 @@
 package com.mwongera.paediatric_protocols.fragment;
 
-import android.app.Activity;
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v7.widget.GridLayoutManager;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.LinearLayout;
 import android.widget.ListView;
 
-import com.github.florent37.materialviewpager.header.MaterialViewPagerHeaderDecorator;
-import com.mwongera.paediatric_protocols.ClickListener;
-import com.mwongera.paediatric_protocols.Main2Activity;
-import com.mwongera.paediatric_protocols.MyAdapter;
+import com.mwongera.paediatric_protocols.DrugDescriptionActivity;
 import com.mwongera.paediatric_protocols.R;
-import com.mwongera.paediatric_protocols.item;
 
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * Created by mwongera on 2/10/17.
