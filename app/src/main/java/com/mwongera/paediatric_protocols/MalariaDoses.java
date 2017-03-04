36ebbd61b85e3aae9fb143941a651fcf27c62943
mwongeraE <evansmwongera@gmail.com>
2017-03-04 23:59:18 +0300

    malariadoses

---
+++ app/src/main/java/com/mwongera/paediatric_protocols/MalariaDoses.java
@@ -0,0 +1,30 @@
+package com.mwongera.paediatric_protocols;
+
+import android.app.Activity;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import uk.co.senab.photoview.PhotoViewAttacher;
+
+/**
+ * Created by mwongera on 3/4/17.
+ */
+
+public class MalariaDoses extends Activity {
+
+    ImageView imgv;
+
+    PhotoViewAttacher mAttacher;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_malaria_doses);
+        imgv = (ImageView) findViewById(R.id.imageView1);
+        mAttacher = new PhotoViewAttacher(imgv);
+        getActionBar().setBackgroundDrawable(new ColorDrawable(Color.parseColor("#f9a614")));
+    }
+
+}
