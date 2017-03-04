fc746331ad237c4c7264c1e3f5525a935522337a
mwongeraE <evansmwongera@gmail.com>
2017-03-04 14:17:02 +0300

    feeding activity

--- app/src/main/java/com/mwongera/paediatric_protocols/FeedingActivity.java
+++ app/src/main/java/com/mwongera/paediatric_protocols/FeedingActivity.java
@@ -1,9 +1,11 @@
 package com.mwongera.paediatric_protocols;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
 
 import uk.co.senab.photoview.PhotoViewAttacher;
@@ -24,6 +26,24 @@ public class FeedingActivity extends Activity {
         getActionBar().setBackgroundDrawable(new ColorDrawable(Color.parseColor("#f9a614")));
         imgv = (ImageView) findViewById(R.id.imageView1);
         mAttacher = new PhotoViewAttacher(imgv);
+
+        //onlong press
+        mAttacher.setOnLongClickListener(new View.OnLongClickListener() {
+
+            @Override
+            public boolean onLongClick(View v) {
+                // TODO Auto-generated method stub
+
+                Intent i = new Intent(FeedingActivity.this, MalnutritionFeeding.class);
+
+                startActivity(i);
+
+                return false;
+            }
+
+
+        });
+
 
     }
 }
