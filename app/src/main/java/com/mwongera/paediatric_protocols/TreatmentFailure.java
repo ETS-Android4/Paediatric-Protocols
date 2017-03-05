fb1b3489f5a2c10790a152c6880fc6b567e4edb7
mwongeraE <evansmwongera@gmail.com>
2017-03-05 08:52:26 +0300

    treatment failure

---
+++ app/src/main/java/com/mwongera/paediatric_protocols/TreatmentFailure.java
@@ -0,0 +1,33 @@
+package com.mwongera.paediatric_protocols;
+
+import android.app.Activity;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import uk.co.senab.photoview.PhotoViewAttacher;
+
+/**
+ * Created by mwongera on 3/5/17.
+ */
+
+public class TreatmentFailure extends Activity {
+
+    ImageView imgv;
+
+    PhotoViewAttacher mAttacher;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_treatment_failure);
+        imgv = (ImageView) findViewById(R.id.imageView1);
+        mAttacher = new PhotoViewAttacher(imgv);
+
+        getActionBar().setBackgroundDrawable(new ColorDrawable(Color.parseColor("#f9a614")));
+
+
+    }
+
+}
