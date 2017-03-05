baba45da60987aac8715ac951200f4d5aa2bf818
mwongeraE <evansmwongera@gmail.com>
2017-03-05 08:28:07 +0300

    plan A activity

---
+++ app/src/main/java/com/mwongera/paediatric_protocols/PlanAActivity.java
@@ -0,0 +1,85 @@
+package com.mwongera.paediatric_protocols;
+
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.webkit.WebView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+/**
+ * Created by mwongera on 3/5/17.
+ */
+
+public class PlanAActivity extends Activity {
+
+    Button btn,btn1;
+    TextView results;
+    EditText inputweight;
+    WebView webv;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_plan_a);
+        getActionBar().setBackgroundDrawable(new ColorDrawable(Color.parseColor("#f9a614")));
+        final String customHtml ="<html><heading>Plan A </heading><p> 1) Continue breast feeding and encourage feeding if > 6 months";
+        webv=(WebView)findViewById(R.id.webView1);
+
+        results=(TextView)findViewById(R.id.textResults);
+
+        inputweight=(EditText)findViewById(R.id.weight);
+        btn=(Button)findViewById(R.id.button1);
+        btn1=(Button)findViewById(R.id.button2);
+        btn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                Double weight;
+                Double resulting;
+
+                if (inputweight.getText().toString().trim().length()!=0){
+                    removetheKeyboard();
+                    weight= Double.parseDouble(inputweight.getText().toString());
+                    resulting=weight*10;
+                    results.setText(resulting+ " mls ORS after each stool ");
+                    //notes.loadData(Noted, "text/html", "UTF-8");
+                    webv.loadData(customHtml, "text/html", "UTF-8");
+
+                }else{
+
+                    Toast.makeText(getApplicationContext(), "Input the Weight", Toast.LENGTH_SHORT).show();
+
+                }
+
+
+            }
+        });
+        btn1.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                results.setText("");
+                inputweight.setText("");
+                webv.clearView();
+
+            }
+        });
+
+    }
+    public void removetheKeyboard(){
+        //First remove the virtual keyboad.
+
+
+        InputMethodManager imm = (InputMethodManager)this.getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(inputweight.getWindowToken(), 0);
+    }
+
+}
