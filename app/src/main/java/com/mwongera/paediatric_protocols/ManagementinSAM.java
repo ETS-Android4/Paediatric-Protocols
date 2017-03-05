cf9b476b5d09a419422e82a3d1e6600ec3f12663
mwongeraE <evansmwongera@gmail.com>
2017-03-05 07:54:46 +0300

    managementinSAM

---
+++ app/src/main/java/com/mwongera/paediatric_protocols/ManagementinSAM.java
@@ -0,0 +1,31 @@
+package com.mwongera.paediatric_protocols;
+
+import android.app.Activity;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import uk.co.senab.photoview.PhotoViewAttacher;
+
+/**
+ * Created by mwongera on 3/5/17.
+ */
+
+public class ManagementinSAM extends Activity {
+
+    ImageView imgv;
+
+    PhotoViewAttacher mAttacher;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_managementin_sam);
+        imgv = (ImageView) findViewById(R.id.imageView1);
+        mAttacher = new PhotoViewAttacher(imgv);
+        getActionBar().setBackgroundDrawable(new ColorDrawable(Color.parseColor("#f9a614")));
+    }
+
+
+}
