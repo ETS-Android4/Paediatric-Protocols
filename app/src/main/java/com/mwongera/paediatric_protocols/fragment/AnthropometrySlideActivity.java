f8f0a84ac4c02a5030d9965e73b84a587059cc3b
mwongeraE <evansmwongera@gmail.com>
2017-03-21 23:36:39 +0300

    Anthropometry slide activity

---
+++ app/src/main/java/com/mwongera/paediatric_protocols/fragment/AnthropometrySlideActivity.java
@@ -0,0 +1,11 @@
+package com.mwongera.paediatric_protocols.fragment;
+
+import android.support.v4.app.FragmentActivity;
+
+/**
+ * Created by mwongera on 3/21/17.
+ */
+
+public class AnthropometrySlideActivity extends FragmentActivity {
+
+}
