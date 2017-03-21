9cefce4e78c12a3857156ab074bf9ccfca81ed39
mwongeraE <evansmwongera@gmail.com>
2017-03-21 13:32:09 +0300

    activity screen slide xml

--- app/src/main/java/com/mwongera/paediatric_protocols/fragment/HivSlideActivity.java
+++ app/src/main/java/com/mwongera/paediatric_protocols/fragment/HivSlideActivity.java
@@ -1,8 +1,26 @@
 package com.mwongera.paediatric_protocols.fragment;
 
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+
+import com.mwongera.paediatric_protocols.R;
+
 /**
  * Created by mwongera on 3/21/17.
  */
 
-public class HivSlideActivity {
+public class HivSlideActivity extends FragmentActivity {
+
+    private static final int NUM_PAGES = 2;
+    private ViewPager mPager;
+    private PagerAdapter mPagerAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_screen_slide);
+    }
+
 }
