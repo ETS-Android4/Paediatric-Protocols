1c4b8e293f23fba7432ed9a0203f8a454130ec12
mwongeraE <evansmwongera@gmail.com>
2017-03-22 00:05:10 +0300

    Anthropometry weights

--- app/src/main/java/com/mwongera/paediatric_protocols/fragment/AnthropometrySlideActivity.java
+++ app/src/main/java/com/mwongera/paediatric_protocols/fragment/AnthropometrySlideActivity.java
@@ -1,11 +1,135 @@
 package com.mwongera.paediatric_protocols.fragment;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.app.NavUtils;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.mwongera.paediatric_protocols.MainActivity;
+import com.mwongera.paediatric_protocols.R;
 
 /**
  * Created by mwongera on 3/21/17.
  */
 
 public class AnthropometrySlideActivity extends FragmentActivity {
+
+    private static final int NUM_PAGES = 6;
+    private ViewPager mPager;
+    private PagerAdapter mPagerAdapter;
+
+    private WFH0_fragment mWFH0;
+    private WeightEstimationFragment mWeightestimation;
+    private WFH1_fragment mWFH1;
+    private WFH2_fragment mWFH2;
+    private WFH3_fragment mWFH3;
+    private WFH4_fragment mWFH4;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_screen_slide);
+
+        //Instantiate a ViewPager and a PagerAdapter
+        mPager = (ViewPager) findViewById(R.id.pager);
+        mPagerAdapter = new AnthropometrySlideActivity.ScreenSlidePagerAdapter(getSupportFragmentManager());
+        mPager.setAdapter(mPagerAdapter);
+        mPager.setOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener() {
+
+            @Override
+            public void onPageSelected(int position) {
+                invalidateOptionsMenu();
+            }
+
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.activity_screen_slide, menu);
+
+        menu.findItem(R.id.action_previous).setEnabled(mPager.getCurrentItem() > 0);
+
+        MenuItem item = menu.add(Menu.NONE, R.id.action_next, Menu.NONE,
+                (mPager.getCurrentItem() == mPagerAdapter.getCount() - 1)
+                        ? R.string.action_finish
+                        : R.string.action_next);
+        item.setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM | MenuItem.SHOW_AS_ACTION_WITH_TEXT);
+        return true;
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                // Navigate "up" the demo structure to the launchpad activity.
+                // See http://developer.android.com/design/patterns/navigation.html for more.
+                NavUtils.navigateUpTo(this, new Intent(this, MainActivity.class));
+                return true;
+
+            case R.id.action_previous:
+                // Go to the previous step in the wizard. If there is no previous step,
+                // setCurrentItem will do nothing.
+                mPager.setCurrentItem(mPager.getCurrentItem() - 1);
+                return true;
+
+            case R.id.action_next:
+                // Advance to the next step in the wizard. If there is no next step, setCurrentItem
+                // will do nothing.
+                mPager.setCurrentItem(mPager.getCurrentItem() + 1);
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private class ScreenSlidePagerAdapter extends FragmentStatePagerAdapter {
+        public ScreenSlidePagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            switch(position){
+                case 0:
+                    mWFH0 = new WFH0_fragment();
+                    return WFH0_fragment;
+                case 1:
+                    mWeightestimation = new WeightEstimationFragment();
+                    return WeightEstimationFragment;
+                case 2:
+                    mWFH1 = new WFH1_fragment();
+                    return WFH1_fragment;
+                case 3:
+                    mWFH2 = new WFH2_fragment();
+                    return WFH2_fragment;
+                case 4:
+                    mWFH3 = new WFH3_fragment();
+                    return WFH3_fragment;
+                case 5:
+                    mWFH4 = new WFH4_fragment();
+                    return WFH4_fragment;
+
+                default: break;
+
+            }
+            return null;
+        }
+
+        @Override
+        public int getCount() {
+            return NUM_PAGES;
+        }
+    }
+
 
 }
